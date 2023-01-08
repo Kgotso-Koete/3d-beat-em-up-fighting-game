2b994ac023761be35821b1906bff910cc33f3481
Kgotso Koete <kgkoete@gmail.com>
2023-01-08 11:03:05 +0000

    enemy movement script

---
+++ Assets/Scripts/Enemy Scripts/EnemyMovement.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// saved place on YouTube = https://youtu.be/gcF66q-UPCs?t=7101
+public class EnemyMovement : MonoBehaviour
+{
+    private CharacterAnimation enemyAnim;
+    private Rigidbody myBody;
+    public float speed = 5f;
+    private Transform playerTarget;
+    public float attack_Distance = 1f;
+    private float chase_Player_After_Attack = 1f;
+    private float current_Attack_Time;
+    private float default_Attack_Time = 2f;
+    private bool followPlayer, attackPlayer;
+    void Awake()
+    {
+        enemyAnim = GetComponentInChildren<CharacterAnimation>();
+        myBody = GetComponent<Rigidbody>();
+        playerTarget = GameObject.FindGameObjectWithTag(Tags.PLAYER_TAG).transform;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        followPlayer = true;
+        current_Attack_Time = default_Attack_Time;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Attack();
+    }
+    void FixedUpdate()
+    {
+        FollowTarget();
+    }
+    void FollowTarget()
+    {
+        if(!followPlayer)
+        {
+            return;
+        }
+        // walk to player if out of attack range from player
+        if(Vector3.Distance(transform.position,playerTarget.position) > attack_Distance)
+        {
+            // look at player
+            transform.LookAt(playerTarget);
+            // go towards them at designated speed
+            myBody.velocity = transform.forward * speed;
+            // if the character is not stationary
+            if(myBody.velocity.sqrMagnitude != 0)
+            {
+                enemyAnim.Walk(true);
+            }
+        }
+        else if(Vector3.Distance(transform.position,playerTarget.position) <= attack_Distance)
+        {
+            // stop any movement
+            myBody.velocity = Vector3.zero;
+            enemyAnim.Walk(false);
+            followPlayer = false;
+            attackPlayer = true;
+        }
+    }
+    void Attack()
+    {
+        if(!attackPlayer)
+        {
+            return;
+        }
+        current_Attack_Time += Time.deltaTime;
+        // keep attacking while attack time has not run out
+        if(current_Attack_Time > default_Attack_Time)
+        {
+            // attack animation 0, 1, or 2
+            enemyAnim.EnemyAttack(Random.Range(0,3));
+            current_Attack_Time = 0f;
+        }
+        if(Vector3.Distance(transform.position,playerTarget.position) > (attack_Distance + chase_Player_After_Attack))
+        {
+            attackPlayer = false;
+            followPlayer = true;
+        }
+    }
+}
--- Assets/Scripts/Player Scripts/CharacterAnimation.cs
+++ Assets/Scripts/Player Scripts/CharacterAnimation.cs
@@ -40,4 +40,39 @@ public class CharacterAnimation : MonoBehaviour
     {
         anim.SetTrigger(AnimationTags.KICK_2_TRIGGER);
     }
+    public void EnemyAttack(int attack)
+    {
+        if(attack == 0)
+        {
+            anim.SetTrigger(AnimationTags.ATTACK_1_TRIGGER);
+        }
+        if(attack == 1)
+        {
+            anim.SetTrigger(AnimationTags.ATTACK_2_TRIGGER);
+        }
+        if(attack == 2)
+        {
+            anim.SetTrigger(AnimationTags.ATTACK_3_TRIGGER);
+        }
+    }
+    public void Play_IdleAnimation()
+    {
+        anim.Play(AnimationTags.IDLE_ANIMATION);
+    }
+    public void KnockDown()
+    {
+        anim.SetTrigger(AnimationTags.KNOCK_DOWN_TRIGGER);
+    }
+    public void StandUp()
+    {
+        anim.SetTrigger(AnimationTags.STAND_UP_TRIGGER);
+    }
+    public void Hit()
+    {
+        anim.SetTrigger(AnimationTags.HIT_TRIGGER);
+    }
+    public void Death()
+    {
+        anim.SetTrigger(AnimationTags.DEATH_TRIGGER);
+    }
 }
